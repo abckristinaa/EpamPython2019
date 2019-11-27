3257aa132cee27b9686d5f3c1615bdc95259fb33
Kristina <abckristinaa@gmail.com>
2019-11-27 12:23:29 +0300

    The redundant condition is removed

--- 03-fp-decorator/hw/hw2_is_armstrong.py
+++ 03-fp-decorator/hw/hw2_is_armstrong.py
@@ -1,8 +1,7 @@
 def is_armstrong(number):
     """ Returns True if the given number is Armstrong number, else False.
     """
-    return True if number == sum(map(lambda x: int(x) ** len(str(number)),
-                                     str(number))) else False
+    return number == sum(map(lambda x: int(x) ** len(str(number)),str(number)))
 
 
 assert is_armstrong(153) == True, 'Число Армстронга'
