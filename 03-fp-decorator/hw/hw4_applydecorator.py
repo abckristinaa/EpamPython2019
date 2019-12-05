1d56119e73a4346722f3274d49773a09937dccdd
Kristina <abckristinaa@gmail.com>
2019-12-05 13:22:43 +0300

    Edited version 1

--- 03-fp-decorator/hw/hw4_applydecorator.py
+++ 03-fp-decorator/hw/hw4_applydecorator.py
@@ -2,9 +2,10 @@
 def applydecorator(func):
     """ Makes a decorator from the given function. """
     def inner(f, *args, **kwargs):
-        func(f, *args, **kwargs)
-        f = helper(f)
-        return f
+        def state(*args, **kwargs):
+            helper(f)
+            return func(f, *args, **kwargs)
+        return state
     return inner
 
 
