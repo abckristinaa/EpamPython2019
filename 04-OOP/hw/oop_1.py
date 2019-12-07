22580948e0783c94594a5590cf30916f50771f48
Kristina <abckristinaa@gmail.com>
2019-12-07 23:18:52 +0300

    First commit

--- 04-OOP/hw/oop_1.py
+++ 04-OOP/hw/oop_1.py
@@ -37,23 +37,57 @@ PEP8 соблюдать строго, проверку делаю автотес
 К названием остальных переменных, классов и тд. подходить ответственно -
 давать логичные подходящие имена.
 """
-import datetime
+
+import datetime as dt
 
 
-if __name__ == '__main__':
-    teacher = Teacher('Daniil', 'Shadrin')
-    student = Student('Roman', 'Petrov')
+class Homework:
+    def __init__(self, text, deadline):
+        self.text = text
+        self.created = dt.datetime.now()
+        self.deadline = dt.timedelta(days=deadline)
+
+    def is_active(self):
+        return (self.created + self.deadline) > dt.datetime.now()
+
+
+class Student:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @staticmethod
+    def do_homework(homework):
+        if homework.is_active():
+            return homework
+        else:
+            print("You are late")
+
+
+class Teacher:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @staticmethod
+    def create_homework(text, days_to_work):
+        return Homework(text, days_to_work)
+
+
+if __name__ == "__main__":
+    teacher = Teacher("Daniil", "Shadrin")
+    student = Student("Roman", "Petrov")
     teacher.last_name  # Daniil
     student.first_name  # Petrov
 
-    expired_homework = teacher.create_homework('Learn functions', 0)
+    expired_homework = teacher.create_homework("Learn functions", 0)
     expired_homework.created  # Example: 2019-05-26 16:44:30.688762
     expired_homework.deadline  # 0:00:00
     expired_homework.text  # 'Learn functions'
 
     # create function from method and use it
     create_homework_too = teacher.create_homework
-    oop_homework = create_homework_too('create 2 simple classes', 5)
+    oop_homework = create_homework_too("create 2 simple classes", 5)
     oop_homework.deadline  # 5 days, 0:00:00
 
     student.do_homework(oop_homework)
--- 04-OOP/hw/save_original_info.py
+++ 04-OOP/hw/save_original_info.py
@@ -2,25 +2,35 @@
 Написать декоратор который позволит сохранять информацию из
 исходной функции (__name__ and __doc__), а так же сохранит саму
 исходную функцию в атрибуте __original_func
-
 print_result изменять нельзя, за исключением добавления вашего
 декоратора на строку отведенную под него - замените комментарий
-
-До применения вашего декоратор будет вызываться AttributeError при custom_sum.__original_func
+До применения вашего декоратор будет вызываться AttributeError при
+custom_sum.__original_func
 Это корректное поведение
 После применения там должна быть исходная функция
-
 Ожидаемый результат:
 print(custom_sum.__doc__)  # 'This function can sum any objects which have __add___'
 print(custom_sum.__name__)  # 'custom_sum'
 print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 """
-
 import functools
 
 
+def save_attribute(func):
+    def inner(wrapped):
+        """ Function-wrapper which save some atributes of an original function.
+
+        Sets to wrapped function new atribute:  __original_func = func.
+        Takes __name__ and __doc__ atributes from an original function.
+        """
+        wrapped = functools.update_wrapper(wrapped, func)
+        wrapped.__original_func = func
+        return wrapped
+    return inner
+
+
 def print_result(func):
-    # Place for new decorator
+    @save_attribute(func)
     def wrapper(*args, **kwargs):
         """Function-wrapper which print result of an original function"""
         result = func(*args, **kwargs)
