9fdc22acef24f7ae0d5291fb5a37b62f021e93cb
Kristina <abckristinaa@gmail.com>
2019-12-11 11:48:50 +0300

    First commit

--- 06-advanced-python/hw/task1.py
+++ 06-advanced-python/hw/task1.py
@@ -9,10 +9,37 @@ E - dict(<V> : [<V>, <V>, ...])
 class Graph:
     def __init__(self, E):
         self.E = E
+        self.index = -1
+        self.visited = []
+
+    def bfs(self):
+        queue = []
+        for node in self.E:
+            queue.append(node)
+            while queue:
+                node = queue.pop(0)
+                if node not in self.visited:
+                    self.visited.append(node)
+                    queue.extend(self.E[node])
+        return self.visited
+
+    def __iter__(self):
+        self.bfs()
+        return self
+
+    def __next__(self):
+        if self.index == len(self.visited) - 1:
+            self.visited = []
+            raise StopIteration
+        self.index += 1
+        return self.visited[self.index]
 
 
-E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
+E = {'A': ['B', 'C', 'D'],
+     'B': ['C'],
+     'C': [],
+     'D': ['A']}
+
 graph = Graph(E)
-
 for vertex in graph:
     print(vertex)
