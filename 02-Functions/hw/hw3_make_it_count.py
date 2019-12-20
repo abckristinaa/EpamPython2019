9b8d3f0d03d479899ba84f391c2318f732937044
Kristina <abckristinaa@gmail.com>
2019-12-21 01:27:16 +0300

    Edited global to globals()

--- 02-Functions/hw/hw3_make_it_count.py
+++ 02-Functions/hw/hw3_make_it_count.py
@@ -2,10 +2,8 @@ def make_it_count(func, counter_name):
     """ Returns a new function that behave as given function but increments
     counter_name for each сall.
     """
+    def new_func(*args, **kwargs):
+        globals()[counter_name] += 1
+        return func(*args, **kwargs)
 
-    def new_func():
-        global counter_name
-        counter_name += 1
-        return func()
-
-    return new_func()
+    return new_func
