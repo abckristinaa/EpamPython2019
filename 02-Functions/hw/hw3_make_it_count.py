d673fc03ef3e31c6462bd4c4642062f7d0142579
Kristina Mironova <48029150+abckristinaa@users.noreply.github.com>
2019-12-21 12:04:06 +0300

    Update hw3_make_it_count.py

--- 02-Functions/hw/hw3_make_it_count.py
+++ 02-Functions/hw/hw3_make_it_count.py
@@ -1,5 +1,5 @@
 def make_it_count(func, counter_name):
-    """ Returns a new function that behave as given function but increments
+    """ Returns a new function that behave as a given function but increments
     counter_name for each сall.
     """
     def new_func(*args, **kwargs):
