37dd8fd4c89166b55f8f9390efee7f83a070bd19
Kristina <abckristinaa@gmail.com>
2019-12-27 22:56:21 +0300

    Corrected, checked.

--- 02-Functions/hw/hw3_make_it_count.py
+++ 02-Functions/hw/hw3_make_it_count.py
@@ -1,27 +1,25 @@
 
 
-def make_it_count(counter_name):
+def make_it_count(func, counter_name):
     """ Returns a new function that behave as a given function but increments
     counter_name for each сall.
     """
-    def inner(func):
-        def new_func(*args, **kwargs):
-            globals()['counter_name'] += 1
-            return func(*args, **kwargs)
-        return new_func
-    return inner
+    def new_func(*args, **kwargs):
+        globals()[counter_name] += 1
+        return func(*args, **kwargs)
+    return new_func
 
 
 if __name__ == "__main__":
     counter_name = 0
 
-    @make_it_count(counter_name)
     def my_func():
         print('Ola-la!')
 
-
-    my_func()
-    my_func()
-    my_func()
-    my_func()
+    count = make_it_count(my_func, 'counter_name')
+    count()
+    count()
+    count()
+    count()
+    count()
     print(counter_name)
