132fcbeee6ae20522c42c795106bce44b6a14100
Kristina <abckristinaa@gmail.com>
2019-12-27 21:51:54 +0300

    Clarification for non-attrubbutes object cases

--- 07-python-for-advanced/hw/test_siam_class_meta.py
+++ 07-python-for-advanced/hw/test_siam_class_meta.py
@@ -22,7 +22,7 @@ def test_passing_no_arguments():
     assert object_
 
 
-def test_objects_with_same_positinal_arguments():
+def test_objects_with_same_positinal_arguments_are_the_same():
     obj1 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
                    'b', 438, [5, 4, [6, 8]])
     obj2 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
@@ -32,7 +32,7 @@ def test_objects_with_same_positinal_arguments():
     assert obj1 is obj2 is obj3
 
 
-def test_objects_with_same_keyword_arguments():
+def test_objects_with_same_keyword_arguments_are_the_same():
     obj1 = SiamObj(uru=158, z=[1, 2, 3], tr=None, auto=(12, 'dsd', 45), a=0,
                    mem={999: {888: {777: [1, 2]}}})
     obj2 = SiamObj(uru=158, z=[1, 2, 3], tr=None, auto=(12, 'dsd', 45), a=0,
@@ -42,7 +42,7 @@ def test_objects_with_same_keyword_arguments():
     assert obj1 is obj2 is obj3
 
 
-def test_objects_with_same_positional_and_keyword_arguments():
+def test_objects_with_same_positional_and_keyword_arguments_are_the_same():
     obj1 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
                    'b', 438, [5, 4, [6, 8]], uru=158, z=[1, 2, 3], tr=None,
                    auto=(12, 'dsd', 45), a=0,mem={999: {888: {777: [1, 2]}}})
@@ -55,50 +55,15 @@ def test_objects_with_same_positional_and_keyword_arguments():
     assert obj1 is obj2 is obj3
 
 
-def test_objects_with_same_no_arguments():
+def test_objects_with_same_no_arguments_are_the_same():
     obj1 = SiamObj()
     obj2 = SiamObj()
     obj3 = SiamObj()
     assert obj1 is obj2 is obj3
 
 
-def test_objects_with_different_positinal_arguments():
-    obj1 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
-                   'b', 438, [5, 4, [6, 8]])
-    obj2 = SiamObj({1: [158, 'a', 4], '1': ('vc', '1s', 0)}, 'a', (13, 18, 16),
-                   'b', 438, [5, 4, [6, 8]])
-    obj3 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
-                   'b', 438, [5, 4, [61, 8]])
-    assert obj1 is not obj2
-    assert obj1 is not obj3
-
-
-def test_objects_with_different_keyword_arguments():
-    obj1 = SiamObj(uru=158, z=[1, 2, 3], tr=None, auto=(12, 'dsd', 45), a=0,
-                   mem={999: {888: {777: [1, 2]}}})
-    obj2 = SiamObj(uru=158, z=[1, 2, 3], tr=None, auto=(12, 'dsd1', 45), a=0,
-                   mem={999: {888: {777: [1, 2]}}})
-    obj3 = SiamObj(uru=158, z=[1, 2, 3], tr=None, auto=(12, 'dsd', 45), a=0,
-                   mem={999: {888: {777: [1, 2, 3]}}})
-    assert obj1 is not obj2
-    assert obj1 is not obj3
-
-
-def test_objects_with_different_positional_and_keyword_arguments():
-    obj1 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
-                   'b', 438, [5, 4, [6, 8]], uru=158, z=[1, 2, 3], tr=None,
-                   auto=(12, 'dsd', 45), a=0,mem={999: {888: {777: [1, 2]}}})
-    obj2 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
-                   'b', 438, [5, 4, [6, 8]], uru=158, z=[1, 21, 3], tr=None,
-                   auto=(12, 'dsd', 45), a=0, mem={999: {888: {777: [1, 2]}}})
-    obj3 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
-                   'b', 438, [5, 4, [6, 8]], uru=158, z=[1, 2, 3], tr=None,
-                   auto=(12, '1dsd', 45), a=0, mem={999: {888: {777: [1, 2]}}})
-    assert obj1 is not obj2
-    assert obj1 is not obj3
-
-
-def test_access_to_other_object_attributes_via_connect():
+def test_access_to_other_object_attributes_via_connect1():
+    # if Siam class has only one object without attributes False is OK
     obj1 = SiamObj({1: [158, 'a', 4], '1': ('vc', 'ds', 0)}, 'a', (13, 18, 16),
                    'b', 438, [5, 4, [6, 8]], uru=158, z=[1, 2, 3], tr=None,
                    auto=(12, 'dsd', 45), a=0, mem={999: {888: {777: [1, 2]}}})
@@ -110,6 +75,24 @@ def test_access_to_other_object_attributes_via_connect():
                             z=[1, 2, 3], tr=None, auto=(12, 'dsd', 45), a=0,
                             mem={999: {888: {777: [1, 2]}}})
     assert obj1 is get_obj1
+
+
+def test_connect_if_different_attr():
+    # if Siam class has only one object without attributes False is OK
+    obj1 = SiamObj('1', '2', b=1)
+    obj2 = SiamObj('1', '2', a=1)
+    obj3 = SiamObj('2', '2', a=1)
+    get_obj = obj3.connect('1', '2', 1)
+    assert get_obj == obj1 or obj2
+
+
+def test_connect_without_attr():
+    # if Siam class has only one object without attributes
+    obj1 = SiamObj('1', '2', b=1)
+    obj2 = SiamObj('1', '2', a=1)
+    obj3 = SiamObj('2', '2', a=1)
+    get_obj = obj3.connect()
+    assert get_obj == obj1 or obj2
 
 
 def test_set_value_to_other_object_attribute():
@@ -135,3 +118,14 @@ def object_deletion():
     assert len(pool) == 3
     del obj3
     assert len(pool) == 2
+
+
+def test_object_deletion_if_no_attributes():
+    # if Siam class has only one object without attributes False is OK
+    obj1 = SiamObj('First object')
+    obj2 = SiamObj('First object')
+    obj3 = SiamObj('Second object')
+    obj4 = SiamObj('Third object')
+    pool = obj3.pool
+    del obj3
+    assert len(pool) == 1
