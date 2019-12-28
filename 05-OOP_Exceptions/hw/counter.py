772b078246b44a10188a87feb2c76c04cf41b673
Kristina <abckristinaa@gmail.com>
2019-12-28 20:45:17 +0300

    Implementation is changed.

--- 05-OOP_Exceptions/hw/counter.py
+++ 05-OOP_Exceptions/hw/counter.py
@@ -11,38 +11,27 @@ reset_instances_counter - сбросить счетчик экземпляров
 
 
 def instances_counter(cls):
-    """ Adds get_created_instances, reset_instances_counter methods to a cls:
+    """ Returns wrapper class. """
 
-    Args:
-        cls: any class.
+    class wrapper:
+        """Adds two methods to the given class. """
+        def __init__(self, *args, **kwargs):
+            cls(*args, **kwargs)
+            cls.instances += 1
 
-    Methods:
-        get_created_instances: returns number of created cls instances.
-        reset_instances_counter: reset number of cls instances.
-      """
+        @staticmethod
+        def get_created_instances():
+            """ Returns number of created class instances."""
+            return cls.instances
+
+        @staticmethod
+        def reset_instances_counter():
+            """ Resets counter of class instances. """
+            result, cls.instances = cls.instances, 0
+            return result
+
     cls.instances = 0
-    call_to_cls_init = cls.__init__
-
-    def __init__(self):
-        """ Executes cls init, adds instances counter attribute. """
-        call_to_cls_init(self)
-        cls.instances += 1
-
-    @classmethod
-    def get_created_instances(cls):
-        """ Returns number of created class instances."""
-        return cls.instances
-
-    @classmethod
-    def reset_instances_counter(cls):
-        """ Resets counter of class instances. """
-        result, cls.instances = cls.instances, 0
-        return result
-
-    cls.__init__ = __init__
-    cls.get_created_instances = get_created_instances
-    cls.reset_instances_counter = reset_instances_counter
-    return cls
+    return wrapper
 
 
 @instances_counter
@@ -56,3 +45,4 @@ if __name__ == "__main__":
     user, _, _ = User(), User(), User()
     print(user.get_created_instances())  # 3
     print(user.reset_instances_counter())  # 3
+    print(User.get_created_instances())  # 0
