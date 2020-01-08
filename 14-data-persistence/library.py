41fa9137d0636543ecd950448922ae089a1263bd
Kristina <abckristinaa@gmail.com>
2020-01-09 01:37:57 +0300

    Update

--- 14-data-persistence/library.py
+++ 14-data-persistence/library.py
@@ -7,25 +7,26 @@ from abc import ABC, abstractmethod
 class Storage(ABC):
     """ Defines essential interface for all kind of storages. """
     @abstractmethod
-    def save_to_library(self, file: str, protocol: str, filename: str):
+    def save(self, file: str, protocol: str, filename: str):
         pass
 
     @abstractmethod
-    def read_from_library(self, file: str, protocol: str, filename: str):
+    def read(self, file: str, protocol: str):
         pass
 
 
 class FileStorage:
     """ Provides save and read file methods with using serialization. """
     @staticmethod
-    def save(file: str, protocol: str, filename: str) -> None:
-        """ Saves a file to library using the given serialization protocol.
+    def save(data, protocol: str, filename: str) -> None:
+        """ Saves a file to a library using the given serialization protocol.
 
         Args:
-            file (str): A name of the file, which will be saved.
+            data: A Python object, which will be saved.
             protocol (str): Pickle or json.
-            filename (str): Output filename.
+            filename (str): Output filename for saved object.
         """
+        filename = filename + '.' + protocol
         if protocol.lower() == 'json':
             flag, protocol = 'w', json
         elif protocol.lower() == 'pickle':
@@ -34,7 +35,7 @@ class FileStorage:
             raise ValueError("Unknown protocol")
 
         with open(filename, flag) as f:
-            protocol.dump(file, f)
+            protocol.dump(data, f)
 
         path_to_file = os.getcwd() + '/' + filename
         if os.path.exists(path_to_file):
@@ -43,13 +44,12 @@ class FileStorage:
             print('\nЧто-то пошло не так. Файл не создан.')
 
     @staticmethod
-    def read(file: str, protocol: str, filename: str):
+    def read(file: str, protocol: str):
         """ Returns a file from file storage available to read.
 
         Args:
             file (str): A name of the file, which will be read.
             protocol (str): Pickle or json.
-            filename (str): Output filename.
         """
         if protocol.lower() == 'json':
             flag, protocol = 'r', json
@@ -59,11 +59,25 @@ class FileStorage:
             raise ValueError("Unknown protocol")
 
         with open(file, flag) as rf:
-            with open(filename, "w") as wf:
-                data = protocol.load(rf)
-                wf.write(data)
+            data = protocol.load(rf)
+        return data
+
+
+
 
 
 if __name__ == "__main__":
-    FileStorage.save("hw.txt", "pickle", "new_file")
-    FileStorage.read("new_file", "pickle", "new_file.txt")
+    class Foo:
+        attr = 'A class attribute'
+
+
+    my_data = {'key': 'This is an object for JSON'}
+
+    FileStorage.save(Foo, "pickle", "new_file")
+    pickle_from_storage = FileStorage.read("new_file.pickle", "pickle")
+    print(pickle_from_storage)
+    print(pickle_from_storage.attr)
+
+    FileStorage.save(my_data, "json", "new_file")
+    json_from_storage = FileStorage.read("new_file.json", "json")
+    print(json_from_storage)
